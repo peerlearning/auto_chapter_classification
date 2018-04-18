a9684ce681bf89dbcc021d717da680a56117ba67
Pritesh Shrivastava <pritesh2312@gmail.com>
2018-04-18 11:36:54 +0530

    Flatten changes

---
+++ qs_topicwise_json.rb
@@ -0,0 +1,42 @@
+require 'active_support/core_ext/object/try' 
+require 'action_view'
+
+results = Array.new
+
+Syllabus.all.select{|s| s.curriculum.present?}.each do |s|
+	s.chapters.each do |c|
+		c.topics.each do |top|
+			top.problems.where(type: "Test").each do |p|
+				if (p.options[1].present?)
+
+					row = Hash[
+						"topic_code" => top.code,
+						"chapter" => c.name,
+						"topic" => top.name, 
+						# p.text.size,
+						# p.solution.size,
+						"difficulty" => p.difficulty_rating,
+						#p.answer,						# Q can have multiple correct answers leading to text overflow
+						# p.tests.first.code,
+						"problem_code" => p.code,
+						"problem_status" => p.review_status,
+						"problem_mongo_id" => p.id.to_s,							# For generating Q links on CMS
+						"option_a" => Rails::Html::FullSanitizer.new.sanitize(p.options[0].text),
+						"option_b" => Rails::Html::FullSanitizer.new.sanitize(p.options[1].text),
+						"option_c" => Rails::Html::FullSanitizer.new.sanitize(p.options[2].text),
+						"option_d" => Rails::Html::FullSanitizer.new.sanitize(p.options[3].text),
+						"question_text" => Rails::Html::FullSanitizer.new.sanitize(p.text)
+						]
+				
+						results.push(row)	
+						
+				end
+			end
+		end
+	end
+end
+
+puts results.to_json
+f = open('qs_topicwise.json', 'w')
+f.write(JSON.pretty_generate(results))
+f.close()
