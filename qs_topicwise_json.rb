4c69ef2fd913005b5d991bd9ad8c650d150ef69d
Pritesh Shrivastava <pritesh2312@gmail.com>
2018-04-19 19:17:42 +0530

    [PS] Add solution and options to document

--- qs_topicwise_json.rb
+++ qs_topicwise_json.rb
@@ -2,18 +2,32 @@ require 'active_support/core_ext/object/try'
 require 'action_view'
 
 results = Array.new
+puts "Starting code...."
+allowedTypes = ["Test", "ConcepTest", "Spot Test", "Homework", "Pre-Reading Exercise", "In Class Test" , "Module Exercise", 
+				"Module Homework", "Pre-Class Exercise", "Pre Test", "In Class Exercise", "Homework", "Activity", 
+				"Recall Test","Miscellaneous", "Illustration", "Module Exercise"
+				]
 
-allowedTypes = ["ConcepTest", "Spot Test", "In Class Exercise", 
-				"Homework", "Test", "Pre-Reading Exercise","Miscellaneous", 
-				"In Class Test" , "Illustration", "Module Exercise", 
-				"Module Homework", "Pre-Class Exercise"]
 
-				
+
 Syllabus.all.select{|s| s.curriculum.present?}.each do |s|
 	s.chapters.each do |c|
 		c.topics.each do |top|
-			top.problems.where(type.in => allowedTypes).each do |p|
-				if (p.options[1].present?)
+			top.problems.where(:type.in => allowedTypes).each do |p|
+			
+				if ( p.text != nil 
+					# p.options[3].present?
+					)
+
+				document_text = p.text
+
+				p.options.each do |op|
+					document_text = document_text + " " + op.text
+				end
+
+				unless p.solution.nil?
+					document_text = document_text + " " + p.solution					
+				end
 
 					row = Hash[
 						"topic_code" => top.code,
@@ -26,15 +40,19 @@ Syllabus.all.select{|s| s.curriculum.present?}.each do |s|
 						# p.tests.first.code,
 						"problem_code" => p.code,
 						"problem_status" => p.review_status,
-						"problem_mongo_id" => p.id.to_s,							# For generating Q links on CMS
-						"option_a" => Rails::Html::FullSanitizer.new.sanitize(p.options[0].text),
-						"option_b" => Rails::Html::FullSanitizer.new.sanitize(p.options[1].text),
-						"option_c" => Rails::Html::FullSanitizer.new.sanitize(p.options[2].text),
-						"option_d" => Rails::Html::FullSanitizer.new.sanitize(p.options[3].text),
-						"question_text" => Rails::Html::FullSanitizer.new.sanitize(p.text)
-						]
+						"problem_mongo_id" => p.id.to_s,				# For generating Q links on CMS
+						"problem_type" => p.type,
+						# "option_a" => Rails::Html::FullSanitizer.new.sanitize(p.options[0].text),
+						# "option_b" => Rails::Html::FullSanitizer.new.sanitize(p.options[1].text),
+						# "option_c" => Rails::Html::FullSanitizer.new.sanitize(p.options[2].text),
+						# "option_d" => Rails::Html::FullSanitizer.new.sanitize(p.options[3].text),
+						# "question_text" => Rails::Html::FullSanitizer.new.sanitize(p.solution),
+						"question_text" => Rails::Html::FullSanitizer.new.sanitize(document_text)
+						 ]
 				
 						results.push(row)	
+				else
+					puts p.id.to_s
 						
 				end
 			end
